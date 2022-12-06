8dbcb4b676cc8dbd855856863626292af685f166
Felipe Pinto <felipe14062000@gmail.com>
2022-12-06 12:38:48 -0300

    Initial commit

---
+++ generators/component.generator.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  description: 'application component',
+  prompts: [
+    {
+      type: 'input',
+      name: 'name',
+      message:
+        'Qual o nome do componente? Caso o mesmo possua mais de uma palavra, separe as pondo um espaço entre elas!'
+    }
+  ],
+  actions: [
+    {
+      type: 'add',
+      path: '../src/components/{{pascalCase name}}/index.js',
+      templateFile: './templates/component/index.js.hbs'
+    },
+    {
+      type: 'add',
+      path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.jsx',
+      templateFile: './templates/component/component.jsx.hbs'
+    },
+    {
+      type: 'add',
+      path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.test.jsx',
+      templateFile: './templates/component/test.jsx.hbs'
+    }
+  ]
+};
